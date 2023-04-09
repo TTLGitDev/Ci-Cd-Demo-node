c8e03a16e16b041f1a8e032a61174d5462b8069a
Shivam Patel <shivam.patel@tatatechnologies.com>
2023-04-10 03:46:48 +0530

    changes

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log('user router')
+  console.log('User router');
   res.send('This is test user response!');
 });
 
